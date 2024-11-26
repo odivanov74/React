59e7a2844b23c52a3603243310ed5ad1f2f3d5aa
odivanov74 <odivanov74@gmail.com>
2024-11-26 21:16:37 +0700

    add function header

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,27 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './Header/header'
 import Article from './article/Article'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello React</h1>
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      </header>
+      <Header/>
       <Article/>
       <Article/>
     </div>
