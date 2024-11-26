60fd3eb9a25fd05c873f360c23ad7d580a5e4e44
odivanov74 <odivanov74@gmail.com>
2024-11-26 20:28:07 +0700

    init

---
+++ intro/src/article/Article.js
@@ -0,0 +1,11 @@
+function Article()
+{
+    return (
+    <article>
+    <p>
+    хорошо живет на свете винипух
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, odit. Sapiente soluta esse tempore accusantium minima iusto odio cupiditate libero, odit est autem ipsa ducimus, nequtetur adipisicing elit. Hic, facilis porro! Ut maxime ad hic molestias ipsum laborum, dolores facere vitae ipsam vero quae beatae est, quaerat aspernature repellat culpa placeat molestias.
+    </p>       
+  </article>)
+}
+export default Article;
