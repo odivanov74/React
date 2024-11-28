f8209a3de1499602cdc81fae403da16d16fa0f48
odivanov74 <odivanov74@gmail.com>
2024-11-28 20:06:46 +0700

    nav

--- intro/src/components/app/App.js
+++ intro/src/components/app/App.js
@@ -1,14 +1,26 @@
 
 import './App.css';
 import Header from '../Header/header';
+import Nav from '../nav/Nav'
 import Article from '../article/Article';
-import Footer from '../footer/Footer';
 import Power from '../power/Power';
+import Footer from '../footer/Footer';
 
 function App() {
+
+  let nav = 
+  {
+    "Главная"   : "/index",
+    "Новости"   : "/news",
+    "Магазин"   : "/shop",
+    "О нас"     : "/about",
+    "Контакты"  : "/contacts",
+  }
+
   return (
     <div className="App">
       <Header title="Hello React" description="This is my first React App. This phrase passed using parameters over 'PROPS'"/>
+      <Nav navigation={nav}/>
       <Power a={2} n={8}/>
       <Article/>
       <Article/>
